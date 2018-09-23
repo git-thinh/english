a99634da64c9abc4d407360d076f31c410db474f
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-09-23 17:17:34 +0700

    c66

---
+++ OpenSources/c66/xilium.cefglue_CEF-3.3359/OffScreenDemo/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Xilium.CefGlue;
+
+namespace OffScreenDemo
+{
+    internal class Program
+    {
+        [STAThread]
+        private static int Main(string[] args)
+        {
+
+            string execDir1 = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string execDir2 = Path.GetDirectoryName(new Uri(System.Reflection.Assembly.GetEntryAssembly().CodeBase).LocalPath);
+
+            //execDir2 = Path.Combine(execDir2, "Debugx86") + @"\";
+            CefRuntime.Load(execDir2);
+
+            var settings = new CefSettings();
+            //settings.MultiThreadedMessageLoop = MultiThreadedMessageLoop = CefRuntime.Platform == CefRuntimePlatform.Windows;            
+            settings.MultiThreadedMessageLoop = true;
+            settings.SingleProcess = true;
+            settings.LogSeverity = CefLogSeverity.Verbose;
+            settings.LogFile = "cef.log";
+            //settings.ResourcesDirPath = Path.GetDirectoryName(new Uri(System.Reflection.Assembly.GetEntryAssembly().CodeBase).LocalPath);
+            settings.ResourcesDirPath = execDir2;
+            settings.RemoteDebuggingPort = 20480;
+            settings.NoSandbox = true;
+
+            ////// Load CEF. This checks for the correct CEF version.
+            ////CefRuntime.Load();
+
+            // Start the secondary CEF process.
+            var cefMainArgs = new CefMainArgs(new string[0]);
+            var cefApp = new DemoCefApp();
+
+            // This is where the code path divereges for child processes.
+            if (CefRuntime.ExecuteProcess(cefMainArgs, cefApp) != -1)
+            {
+                Console.Error.WriteLine("Could not the secondary process.");
+            }
+
+            //var exitCode = CefRuntime.ExecuteProcess(cefMainArgs, cefApp);
+            //Console.WriteLine("CefRuntime.ExecuteProcess() returns {0}", exitCode);
+            //if (exitCode != -1)
+            //    return exitCode;
+
+            ////// Settings for all of CEF (e.g. process management and control).
+            ////var cefSettings = new CefSettings
+            ////{
+            ////    SingleProcess = false,
+            ////    MultiThreadedMessageLoop = true
+            ////};
+
+            // Start the browser process (a child process).
+            ////CefRuntime.Initialize(cefMainArgs, cefSettings, cefApp, IntPtr.Zero);
+            CefRuntime.Initialize(cefMainArgs, settings, cefApp, IntPtr.Zero);
+
+            // Instruct CEF to not render to a window at all.
+            CefWindowInfo cefWindowInfo = CefWindowInfo.Create();
+            //cefWindowInfo.SetAsOffScreen(IntPtr.Zero);
+            cefWindowInfo.SetAsWindowless(IntPtr.Zero, true);
+
+            // Settings for the browser window itself (e.g. enable JavaScript?).
+            var cefBrowserSettings = new CefBrowserSettings();
+
+            // Initialize some the cust interactions with the browser process.
+            // The browser window will be 1280 x 720 (pixels).
+            var cefClient = new DemoCefClient(1280, 720);
+
+            // Start up the browser instance.
+            //CefBrowserHost.CreateBrowser(cefWindowInfo, cefClient, cefBrowserSettings, "http://www.reddit.com/");
+            CefBrowserHost.CreateBrowser(cefWindowInfo, cefClient, cefBrowserSettings, "https://google.com.vn/");
+
+            // Hang, to let the browser to do its work.
+            Console.WriteLine("Press a key at any time to end the program.");
+            Console.ReadKey();
+
+            // Clean up CEF.
+            CefRuntime.Shutdown();
+
+            return 0;
+        }
+    }
+
+}
---
+++ OpenSources/c66/xilium.cefglue_CEF-3.3359/demo/fApp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Xilium.CefGlue.WindowsForms;
+
+namespace demo
+{
+    public partial class fApp : Form
+    {
+        private CefWebBrowser browser;
+        Button btn;
+
+        public fApp()
+        {
+            InitializeComponent();
+
+            browser = new CefWebBrowser
+            {
+                Dock = DockStyle.Fill
+            };
+            browser.Parent = this;
+            browser.BrowserCreated += (se, ev) => {
+                browser.Browser.GetMainFrame().LoadUrl("https://google.com.vn");
+                //browser.Browser.GetMainFrame().LoadUrl("http://localhost:60000/index.html");
+                //browser.Browser.Reload();
+                //browser.Browser.GetMainFrame().LoadUrl("about:blank");
+                //browser.Browser.GetMainFrame().LoadUrl("http://localhost:60000/index.html");
+                //Btn_Click(btn, new EventArgs() { });
+                
+            };
+
+            //btn = new Button() { Text = "Test" };
+            //btn.Click += Btn_Click;
+            //this.Controls.Add(btn);
+            //btn.BringToFront();
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            //browser.Browser.GetMainFrame().LoadUrl(testFileUri.ToString());
+            //browser.Browser.GetMainFrame().LoadUrl("https://google.com.vn");
+            browser.Browser.GetMainFrame().LoadUrl("http://localhost:60000/index.html");
+
+            //Console.WriteLine("Test file loaded.");
+
+            //// wait a bit
+            //for (int i = 0; i < 25; ++i)
+            //{
+            //    Application.DoEvents();
+            //    System.Threading.Thread.Sleep(10);
+            //}
+        }
+
+        private void fApp_Load(object sender, EventArgs e)
+        {
+            this.Width = 1024;
+            this.Height = 600;
+        }
+    }
+}
