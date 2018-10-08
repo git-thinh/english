b948a16a7ce80aecaf508938cf491d347ca63270
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-10-09 05:52:36 +0700

    a

--- Browser/App.cs
+++ Browser/App.cs
@@ -57,27 +57,36 @@ namespace Browser
         static void Main(string[] args)
         {
             string pathRoot = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            string pathCache = Path.Combine(pathRoot, "Cache");
+            string pathBin = Path.Combine(pathRoot, "bin") + @"\";
+
+            string pathCache = Path.Combine(pathBin, "Cache");
             if (!Directory.Exists(pathCache)) Directory.CreateDirectory(pathCache);
 
-            //string pathBin = Path.Combine(pathRoot, "bin") + @"\";
-            //CefRuntime.Load(pathBin);
+            ////////CefSettings settings = new CefSettings() {
+            ////////    CachePath = pathCache,
+            ////////    //LocalesDirPath = pathBin,
+            ////////    //BrowserSubprocessPath = pathBin,
+            ////////    //ResourcesDirPath = pathBin
+            ////////};
+            //////////For Windows 7 and above, best to include relevant app.manifest entries as well
+            ////////Cef.EnableHighDPISupport();
+            //////////We're going to manually call Cef.Shutdown below, this maybe required in some complex scenarios
+            ////////CefSharpSettings.ShutdownOnExit = false;
+            //////////Perform dependency check to make sure all relevant resources are in our output directory.
+            ////////Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
 
-            CefSettings settings = new CefSettings() {
-                CachePath = pathCache,
-                //LocalesDirPath = pathBin,
-                //BrowserSubprocessPath = pathBin,
-                //ResourcesDirPath = pathBin
-            };
 
-            //For Windows 7 and above, best to include relevant app.manifest entries as well
+            CefSettings settings = new CefSettings();
+            settings.CachePath = pathCache;
+            //settings.CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache");
+            //string rootPath = FileUtility.GetAssemblyPath();
+            settings.BrowserSubprocessPath = Path.Combine(pathBin, "CefSharp.BrowserSubprocess.exe");
+            settings.LocalesDirPath = Path.Combine(pathBin, "locales");
+            settings.ResourcesDirPath = Path.Combine(pathBin);
             Cef.EnableHighDPISupport();
-
-            //We're going to manually call Cef.Shutdown below, this maybe required in some complex scenarios
-            CefSharpSettings.ShutdownOnExit = false;
-
-            //Perform dependency check to make sure all relevant resources are in our output directory.
+            settings.CefCommandLineArgs.Add("disable-gpu", "1");
             Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+
 
             var browser = new fBrowser();
             Application.Run(browser);
