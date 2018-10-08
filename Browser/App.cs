70fbcd378da6015ab21d24b5b3afef11ae21f2ba
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-10-09 04:48:23 +0700

    a

--- Browser/App.cs
+++ Browser/App.cs
@@ -1,15 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using CefSharp;
+using CefSharp.WinForms;
+using System.Windows.Forms;
 
 namespace Browser
 {
     class App
     {
+        static App()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += (se, ev) =>
+            {
+                Assembly asm = null;
+                string comName = ev.Name.Split(',')[0];
+
+                ////string resourceName = @"DLL\" + comName + ".dll";
+                ////var assembly = Assembly.GetExecutingAssembly();
+                ////resourceName = typeof(App).Namespace + "." + resourceName.Replace(" ", "_").Replace("\\", ".").Replace("/", ".");
+                ////using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+                using (Stream stream = File.OpenRead("bin/" + comName + ".dll"))
+                {
+                    if (stream == null)
+                    {
+                        //Debug.WriteLine(resourceName);
+                    }
+                    else
+                    {
+                        byte[] buffer = new byte[stream.Length];
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            int read;
+                            while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                                ms.Write(buffer, 0, read);
+                            buffer = ms.ToArray();
+                        }
+                        asm = Assembly.Load(buffer);
+                    }
+                }
+                return asm;
+            };
+        }
+
+
         static void Main(string[] args)
         {
+            string pathCache = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "Cache");
+            if (!Directory.Exists(pathCache)) Directory.CreateDirectory(pathCache);
+
+            CefSettings settings = new CefSettings() { CachePath = pathCache };
+            BrowserSettings browserSettings = new BrowserSettings() { };
+
+            if (!Cef.Initialize(settings))
+            {
+                Console.WriteLine("Couldn't initialise CEF");
+                return;
+            }
+
+            //CEF.RegisterScheme("test", new TestSchemeHandlerFactory());
+            //CEF.RegisterJsObject("bound", new BoundObject());
+
+            //Application.Run(new TabulationDemoForm());
+            Application.Run(new fBrowser());
+
+            Cef.Shutdown();
         }
     }
 }
---
+++ Browser/LIB/Rpc/Interop/Structs/RPC_SERVER_INTERFACE.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Rpc.Interop.Structs
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct RPC_SERVER_INTERFACE
+    {
+        public uint Length;
+        public RPC_SYNTAX_IDENTIFIER InterfaceId;
+        public RPC_SYNTAX_IDENTIFIER TransferSyntax;
+        public IntPtr /*PRPC_DISPATCH_TABLE*/ DispatchTable;
+        public uint RpcProtseqEndpointCount;
+        public IntPtr /*PRPC_PROTSEQ_ENDPOINT*/ RpcProtseqEndpoint;
+        public IntPtr DefaultManagerEpv;
+        public IntPtr InterpreterInfo;
+        public uint Flags;
+
+        public static readonly Guid IID_SYNTAX = new Guid(0x8A885D04u, 0x1CEB, 0x11C9, 0x9F, 0xE8, 0x08, 0x00, 0x2B,
+                                                          0x10,
+                                                          0x48, 0x60);
+
+        public RPC_SERVER_INTERFACE(RpcHandle handle, Ptr<MIDL_SERVER_INFO> pServer, Guid iid)
+        {
+            Length = (uint) Marshal.SizeOf(typeof (RPC_CLIENT_INTERFACE));
+            InterfaceId = new RPC_SYNTAX_IDENTIFIER() {SyntaxGUID = iid, SyntaxVersion = RPC_VERSION.INTERFACE_VERSION};
+            TransferSyntax = new RPC_SYNTAX_IDENTIFIER()
+                                 {SyntaxGUID = IID_SYNTAX, SyntaxVersion = RPC_VERSION.SYNTAX_VERSION};
+
+            RPC_DISPATCH_TABLE fnTable = new RPC_DISPATCH_TABLE();
+            fnTable.DispatchTableCount = 1;
+            fnTable.DispatchTable =
+                handle.Pin(new RPC_DISPATCH_TABLE_Entry()
+                               {DispatchMethod = RpcApi.ServerEntry.Handle, Zero = IntPtr.Zero});
+            fnTable.Reserved = IntPtr.Zero;
+
+            DispatchTable = handle.Pin(fnTable);
+            RpcProtseqEndpointCount = 0u;
+            RpcProtseqEndpoint = IntPtr.Zero;
+            DefaultManagerEpv = IntPtr.Zero;
+            InterpreterInfo = pServer.Handle;
+            Flags = 0x04000000u;
+        }
+    }
+}
